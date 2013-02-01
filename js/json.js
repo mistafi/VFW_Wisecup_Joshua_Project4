bb252e43db7a2ff7f53a8f8d5517a31240cff56f
mistafi <mistafi@gmail.com>
2013-01-31 23:17:28 -0500

    Adding project 4 files

---
+++ js/json.js
@@ -0,0 +1,49 @@
+// JSON Document
+// VFW Project 4 
+// Joshua Wisecup
+// Term 0113 
+
+
+var jsonData = {
+	"pebble1": {
+		"dropdownSelect": ["Type:", "Restaurant"],
+		"inputName": ["Name:", "Johnny's Pizza"],	
+		"inputAddress": ["Address:", "300 W Main St"],	
+		"inputAddress2": ["Address2:", ""],	
+		"inputCity": ["City:", "Frankfort"],	
+		"inputState": ["State:", "KY"],	
+		"inputZip": ["Zip Code:", "40601"],	
+		"inputRating": ["Rating:", "8"],	
+		"inputDate": ["Date of Visit:", "2013-01-30"],	
+		"inputArea": ["Notes:", ""],	
+		"inputCheck": ["Favorite:", "No"]	
+	},
+	"pebble2": {
+		"dropdownSelect": ["Type:", "Gas Station"],
+		"inputName": ["Name:", "Smith Bros."],	
+		"inputAddress": ["Address:", "22 S Main St"],	
+		"inputAddress2": ["Address2:", ""],	
+		"inputCity": ["City:", "Frankfort"],	
+		"inputState": ["State:", "KY"],	
+		"inputZip": ["Zip Code:", "40601"],	
+		"inputRating": ["Rating:", "6"],	
+		"inputDate": ["Date of Visit:", "2013-01-23"],	
+		"inputArea": ["Notes:", ""],	
+		"inputCheck": ["Favorite:", "No"]	
+	},
+	"pebble3": {
+		"dropdownSelect": ["Type:", "Retail Store"],
+		"inputName": ["Name:", "Michael's"],	
+		"inputAddress": ["Address:", "55 Allen Way"],	
+		"inputAddress2": ["Address2:", ""],	
+		"inputCity": ["City:", "Frankfort"],	
+		"inputState": ["State:", "KY"],	
+		"inputZip": ["Zip Code:", "40601"],	
+		"inputRating": ["Rating:", "7"],	
+		"inputDate": ["Date of Visit:", "2013-01-25"],	
+		"inputArea": ["Notes:", ""],	
+		"inputCheck": ["Favorite:", "No"]	
+	}
+	
+	
+}
